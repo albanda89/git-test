63bb5751639d4cea0de4c097c7958cad86abc821
albanda89 <bandaraaruni899@gmail.com>
2022-12-23 23:06:36 +0100

    first sql code

---
+++ test/rockbuster.sql
@@ -0,0 +1,13 @@
+-- Database: Rockbuster
+
+-- DROP DATABASE IF EXISTS "Rockbuster";
+
+CREATE DATABASE "Rockbuster"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
